bcae52c7c9d9daf8c84da7f04eae89b14c8b8adc
Takhirzhon <tashmatov_t@auca.kg>
2024-11-09 04:56:59 +0600

    gwef

--- .github/bin/gendiff.js
+++ .github/bin/gendiff.js
@@ -1,6 +1,6 @@
 import { Command } from 'commander';
 import path from 'path';
-import { parseJsonFile } from '@hexlet/code/parser';
+import { parseJsonFile } from 'parser.js';
 
 const program = new Command();
 
@@ -13,10 +13,10 @@ program
     const absolutePath2 = path.resolve(process.cwd(), filepath2);
 
     try {
-        const data1 = parseJsonFile(absolutePath1);
-        const data2 = parseJsonFile(absolutePath2);
-        console.log('File 1 data:', data1);
-        console.log('File 2 data:', data2);
+      const data1 = parseJsonFile(absolutePath1);
+      const data2 = parseJsonFile(absolutePath2);
+      console.log('File 1 data:', data1);
+      console.log('File 2 data:', data2);
     } catch (error) {
       console.error('Error reading files:', error);
     }
