d0d19a5b74724b8152bca22babb00c59e68a8291
Takhirzhon <tashmatov_t@auca.kg>
2024-11-09 05:27:29 +0600

    Update

--- src/utils.js
+++
@@ -1,5 +0,0 @@
-export const isObject = (obj) => typeof (obj) === 'object' && obj !== null;
-
-export const isEqual = (key, obj1, obj2) => (
-  isObject(obj1[key]) && isObject(obj2[key])
-) || obj1[key] === obj2[key];
